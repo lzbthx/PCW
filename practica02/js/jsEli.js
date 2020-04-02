1aaac95952dbcc644d4c849dea1d76d4168cc85f
Elizabeth <51461252+lzbthx@users.noreply.github.com>
2020-04-02 20:56:03 +0200

    Changeees

--- practica02/js/jsEli.js
+++ practica02/js/jsEli.js
@@ -13,10 +13,6 @@ function hacerLogin(frm){
 		if (r.RESULTADO == 'OK') {
 			console.log(r);
 			sessionStorage['usuario'] = xhr.responseText;
-            sessionStorage['login'] = r.login;
-            sessionStorage['token'] = r.token;
-            sessionStorage['email'] = r.email;
-            sessionStorage['nombre'] = r.nombre;
 
 			mostrarMensaje(r);
 		}else{
@@ -29,19 +25,19 @@ function hacerLogin(frm){
 	return false;
 }
 
+/*
 function comprobar(){
-
 	var comprueba;
 
 	console.log("¿Estamos logueados?");
 
 	if(window.sessionStorage){
 		if(sessionStorage["login"] != null){
-			//if (page == 'login.html'  ||  page == 'registro.html') {
+			if (page == 'login.html'  ||  page == 'registro.html') {
 				getAbsolutePath();
-				//location.href = './index.html';
+				location.href = './index.html';
 				comprueba = true;
-            //}
+            }
 		}else{
 			console.log("No estás logueado. Por favor, inicie sesión.");
 			mostrarMensajeError();
@@ -59,14 +55,14 @@ function getAbsolutePath() {
 
 	console.log(pathName);
 
-    //return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
-}
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}*/
 
 function mostrarMensaje(datos){
     let titulo, texto, div, html;
 
     titulo = 'LOGIN';
-    texto = 'El usuario <b>' + datos.login + '</b> se ha podido loguear de forma correcta';
+    texto = 'El usuario <b>' + datos.login + '</b> se ha logueado de forma correcta';
 
     div = document.createElement('div');
     
@@ -113,6 +109,67 @@ function cerrarError(){
 	document.querySelector('#capa-fondo').remove();
 	document.getElementById('mylog').reset();
 	document.getElementById('login').focus();
+}
+
+function hacerRegistro(frm){
+	let url = 'api/usuarios/registro',
+		fd  = new FormData(frm);
+
+	fetch(url, {method:'POST',body:fd}).then(function(respuesta){
+		if(respuesta.ok){
+			respuesta.json().then(function(datos){
+				console.log(datos);
+				//sessionStorage['usuario'] = JSON.stringify(datos);
+
+				//Texto del mensaje
+				let html = '';
+
+				html += '<article>';
+				html += '<h2>Hacer Login </h2>';
+				html += '<p>El usuario ' + datos.login + ' se ha logueado correctamente</p>';
+				//html += '<p>La operación de login se ha realizado correctamente</p>';
+				html += '<footer><button onclick="cerrarMensajeModal();>Aceptar</button></footer>';
+				html += '</article>';
+
+				mensajeModal(html);
+		});
+		}else
+			console.log('Error en la petición fetch');
+	});
+
+	return false;
+}
+
+function compruebaUsuario(value){
+	let url = `api/usuarios/${value}`,
+	    html;
+
+	fetch(url, {method:'GET'}).then(function(respuesta){
+		if(respuesta.ok){
+			respuesta.json().then(function(datos){
+				//Comprobamos el estado del usuario por consola
+				console.log(datos);
+				if(datos.DISPONIBLE == false){
+					console.log('Usuario no disponible.');
+	
+					html = '<p class="user"> El nombre de usuario no se encuentra disponible. </p>';
+					//document.getElementById("mensaje").innerHTML = "";
+					document.querySelector('#mensaje').innerHTML = html;
+					document.getElementById('mensaje').style.display = "block";
+					document.getElementById('mensaje').style.color = 'red';
+				}else{
+					//No enseñamos ningún mensaje
+					html = '';
+					document.querySelector('#mensaje').innerHTML = html;
+					console.log('Usuario disponible :D');
+				}
+			});
+		}else{
+			// if(response.status!=200)       
+			console.log('Error(' + response.status + '): ' + response.statusText);      
+			return;    
+		}
+	});
 }
 
 function logout(){
