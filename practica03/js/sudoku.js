76dc251d4344cd7181df87517b185102c6054374
Elizabeth <elizabethbalbinm@gmail.com>
2020-04-27 22:39:33 +0200

    Revert "Implementación de rejilla"
    This reverts commit a1a7d59132c4a8f8c43ba0bd17e9a7614fa33684.

--- practica03/js/sudoku.js
+++ practica03/js/sudoku.js
@@ -1,4 +1,3 @@
-var checkbox; 
 
 window.onload = function() {
     if(document.getElementById('inicio')){
@@ -6,51 +5,21 @@ window.onload = function() {
     }
 };
 
-function prepararCanvas(x, y){
-    let cv = document.querySelector('#cv01');
-
-    cv.width = x;
-    cv.height = y;
-}
-
 function iniciar(){
-    console.log('Empieza el juego.');
+    console.log('Hellou');
 
-    let html = '',
-        checkbox = document.querySelector('input[type="radio"]:checked').value;
+    let html = '';
+    //var checkbox = document.getElementById("modo");
+    var text = document.getElementById("text");
 
-    html = '<h2>Panel del juego</h2>';
-    html += '<h3 id="puntua">Puntos: </h3>';
-    html += '<canvas id="cv01"></canvas>';
+    // If the checkbox is checked, display the output text
+    //if (checkbox.checked == true){
+        html = '<h2>Panel del juego</h2>';
+        html += '<h3 id="puntua">Puntos: </h3>';
+        html += '<canvas id="cv01" width="480" height="360"></canvas>';
+    //} else {
+    //    text.style.display = "none";
+    //}
+
     document.getElementById('canvas').innerHTML = html;
-
-    if(checkbox == 4){
-        prepararCanvas(360,360);
-    }else{
-        prepararCanvas(640, 480);
-    }
-
-    rejilla();
-}
-
-function rejilla(){
-    let cv = document.querySelector('canvas'),
-    ctx = cv.getContext('2d'),
-    regiones = 2,
-    ancho = cv.width / regiones,
-    alto = cv.height / regiones;
-
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#a00';
-
-    for(let i = 0; i < regiones; i++){
-        //Verticales
-        ctx.moveTo(i * ancho, 0);
-        ctx.lineTo(i * ancho, cv.height);
-        //Horizontales
-        ctx.moveTo(0, i * alto);
-        ctx.lineTo(cv.width, i * alto);
-    }
-    ctx.stroke();
 }
